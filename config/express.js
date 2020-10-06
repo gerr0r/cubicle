38ee613491c9f854b46e5f9d4943fb5c5ae3274b
gergan ruschev <gergan.ruschev@smartdesign.local>
2020-10-06 15:27:09 +0300

    Create routes. Add templates. Modify hbs files and add partials. Setup view engine and static files.

--- config/express.js
+++ config/express.js
@@ -4,10 +4,15 @@ const bodyParser = require('body-parser');
 
 module.exports = (app) => {
     
-    //TODO: Setup the view engine
+    // Setup the view engine
+    app.engine('.hbs', handlebars({
+        extname: '.hbs'
+    }));
+    app.set('view engine', '.hbs');
 
     //TODO: Setup the body parser
 
     //TODO: Setup the static files
+    app.use(express.static('static'))
 
 };
