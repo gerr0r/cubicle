accd8c7e4823060c87d62028f8c33ce341782a99
GGR <g.ruschev@gmail.com>
2020-10-13 15:46:20 +0300

    Add routes for new pages

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/register", (req, res) => {
+    res.render("register", {
+        title: "Register..."
+    });
+});
+
+router.get("/login", (req, res) => {
+    res.render("login", {
+        title: "Login"
+    });
+});
+
+module.exports = router
